db359e8b186a692bd4a4e1c51c11c2400632f25b
Chiny91 <github@goldhill.co.uk>
2017-08-07 20:15:56 +0100

    Tidying up

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# USBgps
-As per the tin
+# u-blox7 USB GPS
+This is a piece of programming, primarily written to learn Python.
+
+It has the side effect of testing a u-blox7 USB GPS device, ultimately perhaps pretending to be like gpsmon, although of course, far less proficient.
+
+Still, I'm having fun learning about Python, GPS messages and some arcane things about GPS like "hard wired leap seconds".
--- nmea.py
+++ nmea.py
@@ -5,12 +5,14 @@ import curses
 import serial   # pyserial
 import pynmea2  # https://github.com/Knio/pynmea2
 import sys
+from time import gmtime, strftime
 
 def main(stdscr):
     curses.start_color()
     curses.use_default_colors()
     for i in range(0, curses.COLORS):
         curses.init_pair(i + 1, i, -1)
+
     # Set up the USB (serial) port
     port = serial.Serial("/dev/tty.usbmodem1431", 4800, timeout = None)
     while True:
@@ -18,22 +20,35 @@ def main(stdscr):
             response = port.readline()   # read a '\n' terminated line
             msg = pynmea2.parse(response)
             if msg.sentence_type == "GGA":
-                stdscr.addstr(0, 0, str(msg.timestamp), curses.color_pair(2))
-                stdscr.addstr(1, 3, msg.lat, curses.color_pair(1))
-                stdscr.addstr(1, 0, msg.lat_dir, curses.color_pair(1))
-                stdscr.addstr(2, 3, msg.lon, curses.color_pair(1))
-                stdscr.addstr(2, 0, msg.lon_dir, curses.color_pair(1))
-                stdscr.addstr(3, 0, "Altitude", curses.color_pair(1))
-                stdscr.addstr(3, 9, str(msg.altitude), curses.color_pair(1))
-                stdscr.addstr(3, 14, msg.altitude_units, curses.color_pair(1))
-                stdscr.addstr(4, 0, "Number of satellites", curses.color_pair(2))
-                stdscr.addstr(4, 21, msg.num_sats, curses.color_pair(2))
-                stdscr.addstr(5, 0, "", curses.color_pair(1))
+                stdscr.addstr(0, 0, "iMac time:", curses.color_pair(1))
+                stdscr.addstr(0, 11, str(strftime("%H:%M:%S", gmtime())), curses.color_pair(1))
+                stdscr.addstr(1, 0, "GPS time:", curses.color_pair(2))
+                stdscr.addstr(1, 11, str(msg.timestamp), curses.color_pair(2))
+                stdscr.addstr(2, 3, str(msg.latitude), curses.color_pair(2))
+                stdscr.addstr(2, 0, msg.lat_dir, curses.color_pair(2))
+                stdscr.addstr(3, 3, str(msg.longitude), curses.color_pair(2))
+                stdscr.addstr(3, 0, msg.lon_dir, curses.color_pair(2))
+                stdscr.addstr(4, 0, "Altitude", curses.color_pair(2))
+                stdscr.addstr(4, 9, str(msg.altitude), curses.color_pair(2))
+                stdscr.addstr(4, 14, msg.altitude_units, curses.color_pair(2))
+                stdscr.addstr(5, 0, "Number of satellites in use:", curses.color_pair(2))
+                stdscr.addstr(5, 30, msg.num_sats, curses.color_pair(2))
                 stdscr.refresh()
-        except ParseError:    # Ignore any errors from pynmea2
+            if msg.sentence_type == "GSV":
+                stdscr.addstr(6, 0, "Number of satellites in view:", curses.color_pair(2))
+                stdscr.addstr(6, 30, str(msg.num_sv_in_view), curses.color_pair(2))
+                stdscr.addstr(9, 0, "Message Number:", curses.color_pair(1))
+                stdscr.addstr(9, 16, str(msg.msg_num), curses.color_pair(1))
+                stdscr.addstr(10, 0, "Number of messages in GSV cycle:", curses.color_pair(1))
+                stdscr.addstr(10, 33, str(msg.num_messages), curses.color_pair(1))
+                stdscr.addstr(11, 0, "", curses.color_pair(1))
+                stdscr.refresh()
+            # Ignore RMC, VTG, GLL messages as no additional information
+            # Ignore TXT message as never changes
+        except pynmea2.ParseError:
             pass
         except:
-            print "Unexpected error:", sys.exc_info()[0]
+            print "Nobody expects an inquistion about this error:", sys.exc_info()[0]
             raise
 
 # Initialize curses and call another function,
